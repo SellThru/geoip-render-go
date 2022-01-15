14e061fb838f5b98620748954ed357caaa4fe810
Kirk Morales <kirk.morales@gmail.com>
2022-01-14 20:45:13 -0700

    feat: return point for ip

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # geoip-render-go
 Golang project for looking up geo from an IP. Meant to be deployed on Render.
 
+## Routes
+
+`/geo/point` takes `ip` as a query parameter and returns the lat/long for that location:
+
+```json
+{
+  "point": [<LAT>,<LON>]
+}
+```
+
+`/geo/zip` takes `ip` as a query parameter and returns the zip for that location:
+
+```json
+{
+  "zip": "85004"
+}
+```
+
 ## Dependencies
 
 In order to use this project, you'll need a copy of your own [Maxmind GeoIP database](https://www.maxmind.com/en/geoip2-services-and-databases). You can sign up for the GeoLite2 database [here](https://www.maxmind.com/en/geolite2/signup?lang=en).
@@ -17,29 +35,6 @@ This project relies on the following environment variables:
 | `MODE`       | The mode to launch the application in.                                     | No      | "release" |
 | `PORT`       | The port for the web service to listen on.                                 | No      | 3000      |
 
-## How to use
-
-### 1. Fork Repository
-
-Fork this repository into your own account.
-
-### 2. Include your Maxmind GeoIP database
-
-Include your Maxmind GeoIP database in the root of the project. For example, `./GeoLite2-City.mmdb`. Commit this to the repository.
-
-### 3. Update code
-
-Update the `handler()` function in `main.go` to make the appropriate query and return the response.
-
-The example in `main.go` uses the IP address provided to make a `City()` query and returns the zip code as JSON.
-
-### 4. Deploy to render
-
-Create a new web service on Render. Use your new repository and set the above environment variables to make them available at run time.
-
-### 5. Make request
-
-The web service listens for requests to `/:ipaddress`.
 
 ## Notes
 
--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-  "context"
-  "errors"
-  "log"
-  "net"
+	"context"
+	"errors"
+	"log"
+	"net"
 	"net/http"
-  "os"
-  "os/signal"
-  "syscall"
-  "time"
-  "github.com/gin-gonic/gin"
-  "github.com/oschwald/geoip2-golang"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/oschwald/geoip2-golang"
 )
 
 var serviceMode string = os.Getenv("MODE")
@@ -21,52 +22,53 @@ var geoDb *geoip2.Reader
 
 func main() {
 
-  if serviceMode == "" {
-    serviceMode = "release"
-  }
+	if serviceMode == "" {
+		serviceMode = "release"
+	}
 
-  if port == "" {
-    port = "3000"
-  }
+	if port == "" {
+		port = "3000"
+	}
 
-  log.Printf("Starting `geoip` service in '%s' mode...\n", serviceMode)
+	log.Printf("Starting `geoip` service in '%s' mode...\n", serviceMode)
 
-  // Set the run mode of gin (release/debug)
-  gin.SetMode(serviceMode)
+	// Set the run mode of gin (release/debug)
+	gin.SetMode(serviceMode)
 
-  router := gin.New()
+	router := gin.New()
 
-  // Recovery middleware recovers from any panics and writes a 500 if there was one.
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	router.Use(gin.Recovery())
 
-  router.GET("/healthz", func (c *gin.Context) {
-    c.String(200, "OK")
-  })
+	router.GET("/healthz", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
 
-	router.GET("/:ip", handler)
+	router.GET("/geo/point", pointHandler)
+	router.GET("/geo/zip", zipHandler)
 
-  srv := &http.Server{
+	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
 	}
 
-  // Open Maxmind database
-  var geoErr error
-  geoDb, geoErr = geoip2.Open(os.Getenv("GEO_FILE"))
+	// Open Maxmind database
+	var geoErr error
+	geoDb, geoErr = geoip2.Open(os.Getenv("GEO_FILE"))
 	if geoErr != nil {
 		log.Fatal(geoErr)
 	}
 	defer geoDb.Close()
 
-  // Start webserver in background to allow for graceful shutdown code below
-  go func() {
-    log.Printf("Listening on port %v...\n", port)
-    if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
-      log.Panicln(err.Error());
+	// Start webserver in background to allow for graceful shutdown code below
+	go func() {
+		log.Printf("Listening on port %v...\n", port)
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Panicln(err.Error())
 		}
-  }()
+	}()
 
-  // Wait for interrupt signal to gracefully shutdown the server with
+	// Wait for interrupt signal to gracefully shutdown the server with
 	// a timeout of 5 seconds.
 	quit := make(chan os.Signal)
 	// kill (no param) default send syscall.SIGTERM
@@ -74,7 +76,7 @@ func main() {
 	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-  log.Println("Shutting down server...")
+	log.Println("Shutting down server...")
 
 	// The context is used to inform the server it has 5 seconds to finish
 	// the request it is currently handling
@@ -82,31 +84,47 @@ func main() {
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
-    log.Panicf("Server forced to shutdown: %s\n", err.Error())
+		log.Panicf("Server forced to shutdown: %s\n", err.Error())
 	}
 
-  log.Println("Server exiting")
+	log.Println("Server exiting")
 }
 
-func handler(c *gin.Context) {
-
-	ip := net.ParseIP(c.Param("ip"))
-  if ip == nil {
-    c.AbortWithStatus(400)
-    return
-  }
-
-  // Update below this line to get the data you want
+// Gets the city record for the request context. If successful,
+// returns the `*geoip2.City` record and true. If there's a failure,
+// the request is ended directly and the second parameter returned
+// is false.
+func getCityRecord(c *gin.Context) (*geoip2.City, bool) {
+	ip := net.ParseIP(c.Query("ip"))
+	if ip == nil {
+		c.AbortWithStatus(400)
+		return nil, false
+	}
 
 	record, err := geoDb.City(ip)
 	if err != nil {
 		log.Fatal(err)
-    c.AbortWithStatus(500)
-    return
+		c.AbortWithStatus(500)
+		return nil, false
 	}
 
-  c.JSON(200, gin.H{
-		"zip": record.Postal.Code,
-	})
+	return record, true
+}
 
+// Returns the zip code for the IP address in the request
+func zipHandler(c *gin.Context) {
+	if record, ok := getCityRecord(c); ok {
+		c.JSON(200, gin.H{
+			"zip": record.Postal.Code,
+		})
+	}
+}
+
+// Returns the lat/lon point for the IP address in the request
+func pointHandler(c *gin.Context) {
+	if record, ok := getCityRecord(c); ok {
+		c.JSON(200, gin.H{
+			"point": []float64{record.Location.Latitude, record.Location.Longitude},
+		})
+	}
 }
